c34a602293c796aee0fd489efb038838ef44aadf
nicolasnevesdg <onicolasneves@gmail.com>
2026-02-19 19:45:53 -0300

    Add files via upload

---
+++ GUGETFIN/dados.js
@@ -0,0 +1,22 @@
+const bancoInicial = {
+  "config": {
+    "categorias": [
+      "Alimentação",
+      "Assinaturas",
+      "Lazer",
+      "Outros",
+      "Transporte",
+      "Presentes",
+      "Saúde/Estética",
+      "Compras",
+      "Mercado",
+      "Fixos",
+      "Terceiros"
+    ],
+    "detalhesBancos": [],
+    "bancos": []
+  },
+  "entradas": [],
+  "transacoes": [],
+  "metas": []
+};
